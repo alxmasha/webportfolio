5494bf370b501ad8bfdff0fccb989b2e5d1a829b
Maria <43053041+alxmasha@users.noreply.github.com>
2026-01-13 19:40:06 +0000

    fix astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind'; // This is what was crashing
+import tailwind from '@astrojs/tailwind'; // THIS LINE IS LIKELY MISSING
 
 export default defineConfig({
   site: 'https://alxmasha.github.io',
-  base: 'webportfolio', 
-  integrations: [tailwind()],
+  base: 'webportfolio',
+  integrations: [tailwind()], // Now 'tailwind' will be defined
 });
