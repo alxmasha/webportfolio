239baba178c89fd98889023e6e483b7b4e48acab
Maria <43053041+alxmasha@users.noreply.github.com>
2026-01-14 23:16:07 +0000

    add content and style to index

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind'; // THIS LINE IS LIKELY MISSING
-
+import tailwind from '@astrojs/tailwind'; 
 export default defineConfig({
   site: 'https://alxmasha.github.io',
   base: '/webportfolio',
-  integrations: [tailwind()], // Now 'tailwind' will be defined
+  integrations: [tailwind()],
 });
